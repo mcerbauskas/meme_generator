21e1c2094f3ce9ad0fae3f89a1052751f62c4aaf
marius <cerbauskas.marius@gmail.com>
2019-11-11 22:29:13 +0200

    2nd update in server.js

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const app = express();
 const path = require('path');
 
 //telling express to serve up a set of static files that are created when our dist dir is created when the app is built
-app.use(express.static('/dist/meme_generator'));
+//app.use(express.static(__dirname + '/dist/meme_generator'));
+app.use(express.static(path.join(__dirname, 'dist','meme_generator'))); 
 
 //start the app by listening to heroku port
 app.listen(process.env.PORT || 8080);
@@ -12,7 +13,8 @@ app.listen(process.env.PORT || 8080);
 //when we type some subroute of our app we make sure that our index.html file /
 // gets served for any other route that gets typed in the address bar
 app.get('/*', function(req, res) {
-    res.sendFile(path.join('/dist/meme_generator/index.html'));
+    //res.sendFile(path.join(__dirname + '/dist/meme_generator/index.html'));
+    res.sendFile(path.join(__dirname,'dist','meme_generator','index.html'));
 });
 
 console.log('Console listening.');
